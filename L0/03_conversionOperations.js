408e7d49cfec69cb63088edc3e84e1b7732f491a
nimish <gigrasnimish@gmail.com>
2025-10-18 03:30:48 +0530

    conversion operations

---
+++ L0/03_conversionOperations.js
@@ -0,0 +1,78 @@
+// scenarios 1 ( around numbers)
+// 1
+// let score = "33"
+// console.log(typeof score);
+// console.log(typeof(score));
+// let convertScore = Number(score) //typecast integer string into number datatype
+// console.log(convertScore); // 33
+// console.log(typeof convertScore); // number as here "33" was converted to 33 using typecasting
+
+// // 2
+// score = "33abc" // "33abc" invalid value when converted to number
+// convertScore = Number(score) // typecast this invalid value to NaN => (Not a Number) 
+// // Nan belongs to Number datatype
+// console.log(convertScore); // NaN
+// console.log(typeof convertScore); // number
+
+// // 3 (same as in scenario 2)
+// score = undefined 
+// convertScore = Number(score) 
+// console.log(convertScore); // NaN
+// console.log(typeof convertScore); //number
+
+// // 4
+// score = null
+// convertScore = Number(score) // null is represented as 0 when typecasted into number datatype
+// console.log(convertScore); // 0
+// console.log(typeof convertScore); // number
+
+// // 5
+// score = true  // or false
+// convertScore = Number(score) 
+// console.log(convertScore); // 1 for true and 0 for false
+// console.log(typeof convertScore); // number
+
+
+
+// Scenarios 2(around booleans)
+
+// 1
+// let value = 1
+// let convertValue = Boolean(value)
+// console.log(convertValue); // 1 => true and 0 => false
+// console.log(typeof convertValue); // boolean
+
+
+// 2
+// value = ""
+// convertValue = Boolean(value) 
+// console.log(convertValue); // false since empty string translates to a false value
+// console.log(typeof convertValue); //boolean
+
+// 3
+// value = "abc"
+// convertValue = Boolean(value)
+// console.log(convertValue); // true since a non-emtpy string translates to a true value
+// console.log(typeof convertValue); // boolean
+
+
+// 1 => true / 0 => false
+// "" => fasle / "abc" => true
+
+
+// Scenarios 3(around strings)
+
+// let num = 33 
+// let convertNum = String(num)
+// console.log(convertNum); // 33
+// console.log(typeof convertNum); //string
+
+
+
+
+
+
+
+
+
+
