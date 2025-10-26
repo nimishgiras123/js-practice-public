e8dd2bb56eb1358306ba62001aa8b8398c8cffa1
nimish <gigrasnimish@gmail.com>
2025-10-26 20:17:44 +0530

    added datatype summary file

--- L0/05_datatypeSummary.js
+++ L0/05_datatypeSummary.js
@@ -37,3 +37,35 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack(primitive)  and Heap(Non-primitive)
+
+// let myName = "gecko"
+
+// let myName2 = myName
+// myName2 = "gecko2"
+
+// console.log(myName2); // gecko2
+// console.log(myName); // gecko
+
+
+// let obj1 = {
+//     email : "user@sample.com",
+//     name : "user"
+// }
+
+// console.log(obj1.email); // user@sample.com
+
+// let obj2 = obj1
+// // obj2.email = "user2@sample.com"
+
+// console.log(obj1.email); // user2@sample.com
+// console.log(obj2.email); // user2@sample.com
+
+// Primitives are stored on the stack and copied by value, so assigning one variable to another 
+// creates an independent copy. Objects/arrays/functions are stored in the heap, with stack 
+// holding only references, so multiple variables can point to the same data.
